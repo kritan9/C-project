dd09943873f65a99515b45ff4acd74b576afc444
kritan <kritanbanstola1212@gmail.com>
2018-02-20 16:21:39 +0545

    menu function

--- Main.c
+++ Main.c
@@ -12,6 +12,8 @@
 #include "polynomial.h"
 
 void infinity(int);
+void menu(int n,int m,char*a,char*b,char*c,char*d,char*e,char*g,char*h);
+
 int main(){
     int i,j,k;
     char x;
@@ -20,12 +22,9 @@ int main(){
      char c[]=" 3. Correlation And Equation Of Best fit Line ";
      char d[]=" 4. Data Analysis (Standard Deviation And Error) ";
     char  e[]=" 5. Matrix Problems ";
-    char g[]=" 6.Exit";
-    char f[58];
-    for(i=0;i<57;i++){
-        f[i]='\xec';
-    }
-    f[57]=0;
+    char g[]=" 7.Exit";
+    char l[]=" 6.About";
+
 
 X:
     system("cls");
@@ -35,14 +34,34 @@ X:
     printf("\t\t\t\tProject:- Equation Solver, Data Analyzer And Matrix Solver\n");
     printf("\t\t-----------------------------------------------------------------------------------");
     printf("\n\n\t\t\t\t\t\tSelect an Option\n");
+    menu(28,7,a,b,c,d,e,l,g);
 
-    for(i=0;i<=19;i++){
+    printf("\n\t\t\t");
+    x=getch();
+   if(x=='1') Linear();
+    if(x=='3') regression();
+    if(x=='2') polynomial();
+    if(x=='7') goto Y;
+    goto X;
+    Y:
+    return 0;
+}
+
+void menu(int n,int m,char*a,char*b,char*c,char*d,char*e,char*g,char*h){
+    char f[58];
+    int i,j,k;
+    for(i=0;i<57;i++){
+        f[i]='\xec';
+    }
+    f[57]=0;
+    for(i=0;i<=(3*m+1);i++){
             k=0;
-        printf("\n \t\t\t");
+        printf("\n");
+        for(j=0;j<n;j++) printf(" ");
         for(j=0;j<=60;j++){
 
-            if(j==0 || i==0 || j==60 || i==19) printf("\xdc");
-            else if( i<19 && j==2 &&k==0 ) {
+            if(j==0 || i==0 || j==60 || i==(3*m+1)) printf("\xdc");
+            else if( i<(3*m) && j==2 &&k==0 ) {
                  k=1;j-=1;
 
                  switch(i){
@@ -80,6 +99,12 @@ X:
                   case 17 : printf("%s",g);
                         j+=strlen(g);
                          break;
+                case 18:  printf("%s",f);
+                        j+=strlen(f);
+                        break;
+                  case 20 : printf("%s",h);
+                        j+=strlen(h);
+                         break;
                  }
 
 
@@ -88,16 +113,8 @@ X:
         }
 
     }
-    printf("\n\t\t\t");
-    x=getch();
-   if(x=='1') Linear();
-    if(x=='3') regression();
-    if(x=='2') polynomial();
-    if(x=='6') goto Y;
-    goto X;
-    Y:
-    return 0;
 }
+
 
  void infinity(int n){
         int i,j,k;
